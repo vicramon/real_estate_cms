cbc17c387941d5473d9465c93b8d3718645ceaac
Vic Ramon <vic@vicramon.com>
2014-01-20 15:07:23 -0500

    Add styles, redo nav on super admin dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
-  def not_on_super_admin_dashboard
-    request.path != "/super_admin"
+  def li_link_to(text, path, link_options={})
+    if link_options[:exclude]
+      active = request.path.exclude?(link_options.delete(:exclude).to_s)
+    else
+      active = request.path.include?(path.to_s)
+    end
+    active_class = active ? 'active' : nil
+    content_tag('li', class: [active_class, link_options.delete(:li_class)].compact) do
+      link_to text, path, link_options
+    end
   end
 
 end
