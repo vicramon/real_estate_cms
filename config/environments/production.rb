7828cac42ffdb3738aa08f9372eb530e25c38dbc
Vic Ramon <vic@vicramon.com>
2014-01-20 20:03:35 -0500

    Compile assets properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,8 @@ RealEstateCMS::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile = ['application.css', 'super_admin.css']
+  config.assets.compile = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
